fad1c0542bdef5f1cdee15ff472718aed204413e
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2023-07-22 11:38:30 +0200

    Update pmc_article_fetcher.py

--- pmc_article_fetcher.py
+++ pmc_article_fetcher.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
 class PMCArticleFetcher:
     def __init__(self, PMC_ID: str, quote: str):
@@ -9,13 +9,16 @@ class PMCArticleFetcher:
 
     def get_PMC_article_source(self):
         base_url = "https://www.ncbi.nlm.nih.gov/pmc/articles/"
-        chrome_options = Options()
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--no-sandbox") # linux only
-        #chrome_options.add_argument("--start-maximized")
-        driver = webdriver.Chrome(service=Service('chromedriver'), options=chrome_options)
+
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--headless")
+        #options.binary_location = "/home/muhoss/miniconda3/envs/interactoa_venv/bin/FirefoxApp/firefox"
+        #driver = webdriver.Firefox(service=Service("/home/muhoss/miniconda3/envs/interactoa_venv/bin/geckodriver"), options=options)
+        driver = webdriver.Firefox(options=options)
+        #firefox_options.add_argument("--no-sandbox") # linux only
+        #firefox_options.add_argument("--start-maximized")
         driver.get(f"{base_url}{self._PMC_ID}")
         article_source = driver.execute_script("return document.getElementById('mc').outerHTML;")
         head_source = driver.execute_script("return document.getElementsByTagName('head')[0].innerHTML;")
